377b9b57ea86e511ee1a4beef015591b8c406e30
Adrián Arroyo Calle <adrian.arroyocalle@gmail.com>
2014-04-03 14:55:51 +0200

    Plugin

--- bukkit-plugin/src/main/java/adrianistan/bukkit/plugin/Main.java
+++ bukkit-plugin/src/main/java/adrianistan/bukkit/plugin/Main.java
@@ -1,6 +1,7 @@
 package adrianistan.bukkit.plugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.*;
 
 public final class Main extends JavaPlugin{
 	@Override
@@ -10,5 +11,13 @@ public final class Main extends JavaPlugin{
 	@Override
 	public void onDisable(){
 		getLogger().info("Adrianistan Main Plugin has been disabled");
+	}
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("adrianistan")){
+			sender.sendMessage("Adrianistan Server running CraftBukkit");
+			return true;
+		}
+		return false;
 	}
 }
